770910b2e8c3b797c639d94643505d503eb6239b
Dennis D. Box <dbox@fnal.gov>
2016-06-24 16:02:20 -0500

    syncing stuff up

--- test/server/puppetfiles/modules/dbox-jobsub_server/manifests/packages.pp
+++ test/server/puppetfiles/modules/dbox-jobsub_server/manifests/packages.pp
@@ -1,11 +1,7 @@
-class jobsub_server::packages {
-#   yumrepo { 'jenkins':
-#     baseurl  => 'http://pkg.jenkins-ci.org/redhat',
-#     descr    => 'Jenkins',
-#     enabled  => 1,
-#     gpgcheck => 1,
-#     gpgkey   => 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key',
-#   }
+class jobsub_server::packages (String $ifdhc_version = $jobsub_server::vars::ifdhc_version, 
+                               String $jobsub_tools_version = $jobsub_server::vars::jobsub_tools_version,
+                               String $ups_version = $jobsub_server::vars::ups_version,
+                               String $ups_flavor = $jobsub_server::vars::ups_flavor ) {
     yumrepo { 'jobsub':
       baseurl  => 'http://web1.fnal.gov/files/jobsub/dev/6/x86_64/',
       descr    => 'Jobsub',
@@ -23,16 +19,20 @@ class jobsub_server::packages {
       ensure   => 'installed',
       provider => 'rpm',
       source   => 'https://repo.grid.iu.edu/osg/3.3/osg-3.3-el6-release-latest.rpm',
+      notify   => Exec['yum-clean-all'],
+    }
+
+    exec { 'yum-clean-all':
+      command => '/usr/bin/yum clean all',
     }
 
     package { 'fermilab-util_kx509.noarch' :
-      ensure => 'absent',
+      ensure => 'present',
     }
+
 
     package {'git': ensure => present}
     package { 'httpd': ensure => present}
-#   package {'java-1.8.0-openjdk': ensure =>present}
-#   package { 'jenkins': ensure => present, }
     package { 'upsupdbootstrap-fnal': ensure => present }
 
     package { 'llrun':
@@ -55,10 +55,6 @@ class jobsub_server::packages {
       install_options => '--enablerepo=osg',
     }
 
-    #package { 'dcache-srmclient.noarch':
-    #  ensure          => present,
-    #  install_options => '--enablerepo=osg',
-    #}
       
     package { 'uberftp':
       ensure          => present,
@@ -70,10 +66,6 @@ class jobsub_server::packages {
       install_options => '--enablerepo=osg',
     }
 
-#   package { 'bestman2-client':
-#     ensure          => present,
-#     install_options => '--enablerepo=osg',
-#   }
 
     package { 'condor':
       ensure          => present,
@@ -83,33 +75,23 @@ class jobsub_server::packages {
     package { 'jobsub':
       ensure          => $jobsub_server::vars::jobsub_server_version,
       install_options => '--enablerepo=jobsub',
+      require => Package['voms-clients-cpp'],
+    }
+
+    package { 'voms-clients-cpp':
+      install_options => '--enablerepo=osg-development',
     }
 
     package { 'osg-ca-scripts':
       ensure          => present,
       install_options => '--enablerepo=osg',
     }
-
-    #install jobsub_tools version and make it current
-    $cmd = '/bin/su products -c '
-    $ups = '. /fnal/ups/etc/setups.sh; setup ups; setup upd; '
-    $install = ' upd install '
-    $ver = $jobsub_server::vars::jobsub_tools_version
-    $jt_ver = "jobsub_tools ${ver} -f Linux+2"
-    $declare = ' ups declare -c '
-
-    exec { 'install_jobsub_tools':#                                            #
-      command => "${cmd} \"${ups} ${install} ${jt_ver};${declare} ${jt_ver}\" ",
-      require => [ Package['upsupdbootstrap-fnal'], ],
-      unless  => "${cmd} \"${ups} ups exist ${jt_ver} \" " ,
-    }
-
-    $ifdh_v = "ifdhc v1_8_5 -f Linux64bit+2.6-2.12 -q python27"
-    exec { 'install_ifdhc':
-      command => "${cmd} \"${ups} ${install} ${ifdh_v};${declare} ${ifdh_v}\" ",
-      require => [ Package['upsupdbootstrap-fnal'], ],
-      unless  => "${cmd} \"${ups} ups exist ${ifdh_v} \" " ,
-    }
   
+    #install these ups products and make them current
+    jobsub_server::ups::product {
+      'ups'          : version => $ups_version, qualifier => "-f ${ups_flavor}";
+      'jobsub_tools' : version => $jobsub_tools_version, qualifier => "-f Linux+2" ;
+      'ifdhc'        : version => $ifdhc_version, qualifier => "-f ${ups_flavor} -q python27";
+    }
 
 }
--- test/server/puppetfiles/modules/jobsub_client/manifests/vars.pp
+++ test/server/puppetfiles/modules/jobsub_client/manifests/vars.pp
@@ -1,49 +1,38 @@
 # author Dennis Box, dbox@fnal.gov
-class jobsub_server::vars{
-    $jobsub_server_version = '1.2-3'
-    $jobsub_tools_version = 'v1_4_5'
+class jobsub_client::vars{
+    $jobsub_client_version = 'v1_2_1_5_rc1'
     $jobsub_user = 'rexbatch'
     $jobsub_user_uid = 47535
     $jobsub_group = 'fife'
     $jobsub_user_gid = 9239
     $jobsub_user_home = '/home/rexbatch'
-    $jobsub_basejobsdir = '/fife/local/scratch'
-    $jobsub_logsbasedir = '/fife/local/scratch/logs'
-    $jobsub_jobhistory_count = '30'
-    $jobsub_git_branch = 'puppetized_ci'
-    $jobsub_git_dir = '/var/tmp/jobsub'
-    $jobsub_cert = '/etc/grid-security/jobsub/jobsubcert.pem'
-    $jobsub_key = '/etc/grid-security/jobsub/jobsubkey.pem'
-    ######################################################
-    $jenkins_user = 'jenkins'
-    $jenkins_user_uid = 3617
-    $jenkins_user_gid = $jobsub_user_gid
-    $jenkins_home = '/var/lib/jenkins'
-    $jenkins_cert = '/etc/grid-security/jenkins/jenkinscert.pem'
-    $jenkins_key = '/etc/grid-security/jenkins/jenkinskey.pem'
-    $jenkins_admin_email = 'dbox@fnal.gov'
-    $ups_version = 'v5_1_4'
     $ifdhc_version = 'v1_8_5'
+    $python_version = 'v2_7_3'
+    $pycurl_version = 'v7_15_5'
+    $cigetcert_version = 'v1_0_1'
+    $cigetcert_libs_version = 'v1_1'
+    $kx509_version = 'v3_1_0'
+    $ups_version = 'v5_1_4'
     case $::os['release']['major']{
       '5' : {
         $ups_flavor = 'Linux64bit+2.6-2.5'
         $epel_url = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-5.noarch.rpm'
         $osg_url = 'https://repo.grid.iu.edu/osg/3.2/osg-3.2-el5-release-latest.rpm'
-        $wget_opt = '--no-check-certificate'
+        $wget_opt = '--no-check-certificate' 
         $yum_priorities = 'yum-priorities'
       }
       '6' : {
         $ups_flavor = 'Linux64bit+2.6-2.12'
         $epel_url = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm'
         $osg_url = 'https://repo.grid.iu.edu/osg/3.3/osg-3.3-el6-release-latest.rpm'
-        $wget_opt = ''
+        $wget_opt = '' 
         $yum_priorities = 'yum-plugin-priorities'
       }
       '7' : {
         $ups_flavor = 'Linux64bit+3.10-2.17'
         $epel_url = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
         $osg_url = 'https://repo.grid.iu.edu/osg/3.3/osg-3.3-el7-release-latest.rpm'
-        $wget_opt = ''
+        $wget_opt = '' 
         $yum_priorities = 'yum-plugin-priorities'
       }
       default: {
@@ -53,5 +42,12 @@ class jobsub_server::vars{
         $wget_opt = 'NULL'
       }
     }
-
+    ######################################################
+    $jenkins_user = 'jenkins'
+    $jenkins_user_uid = 3617
+    $jenkins_user_gid = $jobsub_user_gid
+    $jenkins_home = '/var/lib/jenkins'
+    $jenkins_cert = '/etc/grid-security/jenkins/jenkinscert.pem'
+    $jenkins_key = '/etc/grid-security/jenkins/jenkinskey.pem'
+    $jenkins_admin_email = 'dbox@fnal.gov'
 }
